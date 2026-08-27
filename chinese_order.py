115e6691fc6f2678781cbb443f2d39860f83b930
Rashad <r.j.mckenney81@gmail.com>
2026-08-26 20:37:37 -0400

    python file upload

---
+++ chinese_order.py
@@ -0,0 +1,19 @@
+print ("Hello World")
+
+chinese_dish = "Combination Lo Mein"
+dish_count = 2
+price_per_dish = 10
+dinner_budget = 40
+
+
+def print_order_details():
+    print (chinese_dish)
+    print (dish_count)
+    print (price_per_dish)
+    print (dinner_budget)
+    
+print ("function call #1")
+print_order_details()
+
+print ("function call #2")
+print_order_details()
