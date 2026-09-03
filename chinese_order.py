c72f4565c3247e7b9913e87fea24d4e16bbd4e7b
Rashad <r.j.mckenney81@gmail.com>
2026-09-02 21:09:28 -0400

    Added additional funcitons with conditions and return statements

--- chinese_order.py
+++ chinese_order.py
@@ -29,3 +29,39 @@ print("")
 
 print("function call #4")
 print_order_with_vars("BBQ Ribs", 2, 12, 40)
+
+print("")
+
+def calculate_meal_total(dish_count, price_per_dish):
+    total = dish_count * price_per_dish
+    return total
+
+order_total_static = calculate_meal_total(2, 45)
+
+print("function call #5")
+print(f"Total cost of the order with static values: ${order_total_static}")
+
+print("")
+
+order_total_dynamic = calculate_meal_total(dish_count, price_per_dish)
+
+print("function call #6")
+print(f"Total cost of the order with dynamic values: ${order_total_dynamic}")
+
+def decide_purchase(budget, total_cost):
+    if budget > total_cost:
+        return "Purchase approved."
+    elif budget == total_cost:
+        return "Purchase approved (exact amount)."
+    else:
+        return "Purchase denied."
+    
+purchase_decision_static = decide_purchase(40, 40)
+
+print("")
+print("function call #7")
+print(f"Purchase decision with static values: {purchase_decision_static}")
+
+print("")
+purchase_decision_dynamic = decide_purchase(dinner_budget, order_total_dynamic)
+print(f"Purchase decision with dynamic values: {purchase_decision_dynamic}")
